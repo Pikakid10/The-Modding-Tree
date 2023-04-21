231f7a22d3596c117689a1ee91c2df38d84d0fdd
Pikakid10 <87579253+Pikakid10@users.noreply.github.com>
2023-04-21 15:54:29 -0400

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
+	name: "The Tree of Infinite Upgrades",
 	id: "mymod",
-	author: "nobody",
+	author: "Pika_kid10",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
 
