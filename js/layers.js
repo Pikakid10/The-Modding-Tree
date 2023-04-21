49afbdc62650d974af839c19ef4d8000db7118ea
Pikakid10 <87579253+Pikakid10@users.noreply.github.com>
2023-04-21 16:16:00 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -6,7 +6,7 @@ addLayer("p", {
         unlocked: true,
 		points: new Decimal(0),
     }},
-    color: "#000000",
+    color: "#FFFFFF",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
     resource: "coins", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
